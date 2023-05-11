831cd7b32e4fa093ba4a22571835e3f5155b66c6
Garrosh Magrathean <garroshm@student.21-school.ru>
2023-05-11 11:54:53 +0300

    create Router

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <div>Корень</div>
+    },
+    {
+        path: '/:id',
+        element: <div>Stock</div>
+    }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-      <div></div>
-  </React.StrictMode>
+  <RouterProvider router={router}/>
 );
 
 // If you want to start measuring performance in your app, pass a function

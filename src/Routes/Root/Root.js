ba6f3dbaf407a13653646be3c42f846e97e8056a
Garrosh Magrathean <garroshm@student.21-school.ru>
2023-05-14 09:45:12 +0300

    add favicons and titles for pages

--- src/Routes/Root/Root.js
+++ src/Routes/Root/Root.js
@@ -2,8 +2,12 @@ import './Root.css'
 import Header from "../../Base/Header/Header";
 import Footer from "../../Base/Footer/Footer";
 import RootMain from "./RootMain/RootMain";
+import {useEffect} from "react";
 
 const Root = () => {
+    useEffect(() => {
+        document.title = "Rumberg | Главная"
+    }, [])
     return (
         <div className={'root'}>
             <Header/>
--- src/Routes/Stock/Stock.js
+++ src/Routes/Stock/Stock.js
@@ -3,9 +3,13 @@ import Header from "../../Base/Header/Header";
 import Footer from "../../Base/Footer/Footer";
 import StockRoot from "./StockRoot/StockRoot";
 import {useParams} from "react-router-dom";
+import {useEffect} from "react";
 
 const Stock = () => {
     const {id} = useParams()
+    useEffect(() => {
+        document.title = `Rumberg | ${id.toUpperCase()} Index`
+    }, [])
     return (
         <div className={`stock`}>
             <Header/>

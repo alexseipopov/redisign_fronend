20f55eb06b763e76fbcb8648690c17e6a00f6049
Alexsei Popov <popov2895@gmail.com>
2023-10-11 18:14:02 +0300

    change address

--- src/Base/Footer/Footer.js
+++ src/Base/Footer/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
                 <div className={'footer'}>
                     <p className={'footer-content'}>© 2023 Rumberg Technologies. Все права защищены</p>
                     <p className="footer-address">Акционерное общество «Румберг Технолоджис» (АО «Румберг Технолоджис»)
-                        ИНН: 9704196645. Адрес: 19019, г. Москва, ул. Арбат, 6/2, помещ. 1/1/4 оф. 339</p>
+                        ИНН: 9704196645. Адрес: 125040, Г.Москва, вн.тер.г. Муниципальный Округ Беговой, пр-кт Ленинградский, д. 15, стр. 9</p>
                     <a href="mailto:Index@rumberg.ru" className="footer-email">Index@rumberg.ru</a>
                 </div>
             </Container>
